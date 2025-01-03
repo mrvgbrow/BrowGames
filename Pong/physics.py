acde798c8385864cec7b8533dfe507ac6fab599b
Nicholas Bond <sp4cetiger@gmail.com>
2025-01-03 10:33:37 -0500

    Adding a routine to project an angle to the atan2 range.

--- Pong/physics.py
+++ Pong/physics.py
@@ -6,6 +6,9 @@ import math
 def angle_to_coords(angle,amplitude):
     return amplitude*math.cos(angle),amplitude*math.sin(angle)
 
+def angle_to_atan2_range(angle):
+    return math.atan2(math.sin(angle),math.cos(angle))
+
 def coords_to_angle(coordx,coordy):
     return math.atan2(coordy,coordx),(coordx*coordx+coordy*coordy)**0.5
 
