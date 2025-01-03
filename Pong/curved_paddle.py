65c31efe70cfc4dba499f01b28dc78a48650ce7f
Nicholas Bond <sp4cetiger@gmail.com>
2025-01-03 10:07:11 -0500

    Updating the curved paddle to handle multiple control schemes and properly account for its height.

--- Pong/curved_paddle.py
+++ Pong/curved_paddle.py
@@ -12,6 +12,7 @@ class CurvedPaddle(pygame.sprite.Sprite):
         self.surf=pygame.Surface((gc.PLAYER_RADIUS,gc.PLAYER_HEIGHT))
         self.target=-1        
         self.surf.set_colorkey(gc.SCREEN_COLOR)
+        self.real_height=min(gc.PLAYER_HEIGHT,math.sqrt(gc.PLAYER_RADIUS**2-(gc.PLAYER_RADIUS-gc.PLAYER_WIDTH)**2)*2)
         self.rect = self.surf.get_rect(
             center=(
                 x_position,
@@ -21,11 +22,26 @@ class CurvedPaddle(pygame.sprite.Sprite):
         self.draw(color)
 
     def update(self, pressed_keys, control, target,target_speed_y):
-        if control == 'player':
+        if control == 'arrows':
             if pressed_keys[gc.K_UP]:
                 self.rect.move_ip(0,-gc.PLAYER_MOVESTEP)
             if pressed_keys[gc.K_DOWN]:
                 self.rect.move_ip(0,gc.PLAYER_MOVESTEP)
+            if gc.PADDLE_ALLOW_LEFTRIGHT:
+                if pressed_keys[gc.K_LEFT]:
+                    self.rect.move_ip(-gc.PLAYER_MOVESTEP,0)
+                if pressed_keys[gc.K_RIGHT]:
+                    self.rect.move_ip(gc.PLAYER_MOVESTEP,0)
+        elif control == 'wasd':
+            if pressed_keys[gc.K_w]:
+                self.rect.move_ip(0,-gc.PLAYER_MOVESTEP)
+            if pressed_keys[gc.K_s]:
+                self.rect.move_ip(0,gc.PLAYER_MOVESTEP)
+            if gc.PADDLE_ALLOW_LEFTRIGHT:
+                if pressed_keys[gc.K_a]:
+                    self.rect.move_ip(-gc.PLAYER_MOVESTEP,0)
+                if pressed_keys[gc.K_d]:
+                    self.rect.move_ip(gc.PLAYER_MOVESTEP,0)
         elif control == 'computer':
             error_distance=self.compute_error_distance(target_speed_y)
             if (abs(target-self.target)>error_distance and target>0 and target<gc.SCREEN_HEIGHT) or random.random()<gc.AI_RANDOM_ADJUST:
@@ -37,15 +53,20 @@ class CurvedPaddle(pygame.sprite.Sprite):
 
         if self.rect.top <= 0:
             self.rect.top=0
+        if self.rect.left <= 0:
+            self.rect.left=0
         if self.rect.bottom > gc.SCREEN_HEIGHT:
             self.rect.bottom=gc.SCREEN_HEIGHT
+        if self.rect.right > gc.SCREEN_WIDTH:
+            self.rect.right=gc.SCREEN_WIDTH
 
     def compute_anglechange(self,ballpos_y):
         location_on_paddle=ballpos_y-self.rect.centery
-        if abs(location_on_paddle)<gc.PLAYER_RADIUS:
-            nominal_change=math.asin(location_on_paddle/gc.PLAYER_RADIUS)
-        else:
-            nominal_change=0
+        if location_on_paddle>gc.PLAYER_RADIUS:
+            location_on_paddle=gc.PLAYER_RADIUS
+        elif location_on_paddle<-gc.PLAYER_RADIUS:
+            location_on_paddle=-gc.PLAYER_RADIUS
+        nominal_change=2*math.asin(location_on_paddle/gc.PLAYER_RADIUS)
         return nominal_change
     
     def draw(self,color):
@@ -56,6 +77,6 @@ class CurvedPaddle(pygame.sprite.Sprite):
         self.mask=pygame.mask.from_surface(self.surf)
 
     def compute_error_distance(self,ball_speed_y):
-        error_distance= gc.PLAYER_HEIGHT*gc.AI_ERROR_DISTANCE*(1+gc.AI_YSPEED_ERROR_FACTOR*abs(ball_speed_y)/gc.BALL_MAX_SPEED)
+        error_distance= self.real_height*gc.AI_ERROR_DISTANCE*(1+gc.AI_YSPEED_ERROR_FACTOR*abs(ball_speed_y)/gc.BALL_MAX_SPEED)
         return error_distance
 
