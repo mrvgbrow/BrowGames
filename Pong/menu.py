6c3cba9302fbac690a6e9f387dcb12ad19132d91
Nicholas Bond <sp4cetiger@gmail.com>
2025-01-11 09:58:26 -0500

    Moving player control settings to the main menu. Rearranging presets to not be specific to the number of players.

--- Pong/menu.py
+++ Pong/menu.py
@@ -6,6 +6,11 @@ import presets
 
 def run_menu(title,screen,presets_dict,preset_default):
   global menu_run,original_pars
+  control_values=['arrows','wasd','computer']
+  font_size=24
+  input_select_list=[]
+  for input_val in control_values:
+      input_select_list.append((input_val.capitalize(),input_val))
   menu_run=False
   menu=pygame_menu.Menu(title,800,600,theme=pygame_menu.themes.THEME_DEFAULT,onclose=pygame_menu.events.CLOSE)
   get_parameters()
@@ -15,26 +20,29 @@ def run_menu(title,screen,presets_dict,preset_default):
   smenu=pygame_menu.Menu('Settings',800,600,theme=pygame_menu.themes.THEME_DEFAULT,columns=2,rows=n_rows)
   pmenu=pygame_menu.Menu('Presets',800,600,theme=pygame_menu.themes.THEME_DEFAULT,onclose=pygame_menu.events.CLOSE)
   spmenu=pygame_menu.Menu('Save Preset',800,600,theme=pygame_menu.themes.THEME_DEFAULT)
-  menu.add.button('Play with Current Settings',pygame_menu.events.CLOSE)
-  def one_player_run():
-      presets.set_preset(presets_dict['One Player Standard'])
-      menu.close()
-  menu.add.button('One-Player Standard',one_player_run)
-  def two_player_run():
-      presets.set_preset(presets_dict['Two Player Standard'])
-      menu.close()
-  menu.add.button('Two-Player Standard',two_player_run)
-  def zero_player_run():
-      presets.set_preset(presets_dict['Zero Player Standard'])
-      menu.close()
-  menu.add.button('Computer Only',zero_player_run)
-  menu.add.button('Presets',pmenu)
-  menu.add.button('Settings',smenu)
-  menu.add.button('About',amenu)
+  menu.add.button('Play with Current Settings',pygame_menu.events.CLOSE,font_size=font_size)
+#  def one_player_run():
+#      presets.set_preset(presets_dict['One Player Standard'])
+#      menu.close()
+#  menu.add.button('One-Player Standard',one_player_run,font_size=font_size)
+#  def two_player_run():
+#      presets.set_preset(presets_dict['Two Player Standard'])
+#      menu.close()
+#  menu.add.button('Two-Player Standard',two_player_run,font_size=font_size)
+#  def zero_player_run():
+#      presets.set_preset(presets_dict['Zero Player Standard'])
+#      menu.close()
+#  menu.add.button('Computer Only',zero_player_run,font_size=font_size)
+  menu.add.button('Presets',pmenu,font_size=font_size)
+  menu.add.button('Settings',smenu,font_size=font_size)
+  menu.add.button('About',amenu,font_size=font_size)
 #  menu.add.button('Save Preset',spmenu)
-  menu.add.button('Quit',pygame_menu.events.EXIT)
-  menu.add.label('')
-  preset_input=menu.add.label('Loaded preset: '+preset_default)
+  menu.add.button('Quit',pygame_menu.events.EXIT,font_size=font_size)
+  menu.add.label('',font_size=font_size)
+  widget=menu.add.dropselect(title='P1: ',items=input_select_list,onchange=set_input_drop,args=['PLAYER1_CONTROL'],default=control_values.index(original_pars['PLAYER1_CONTROL']),font_size=font_size-2,align=pygame_menu.locals.ALIGN_CENTER)
+  widget=menu.add.dropselect(title='P2: ',items=input_select_list,onchange=set_input_drop,args=['PLAYER2_CONTROL'],default=control_values.index(original_pars['PLAYER2_CONTROL']),font_size=font_size-2,align=pygame_menu.locals.ALIGN_CENTER)
+  menu.add.label('',font_size=font_size)
+  preset_input=menu.add.label('Loaded preset: '+preset_default,font_size=font_size)
   make_amenu(amenu)
   make_smenu(smenu)
   make_pmenu(pmenu,presets_dict,preset_input)
@@ -90,10 +98,6 @@ def make_smenu(smenu):
   global original_pars
   font_size=17
   first_widget=[]
-  input_values=['arrows','wasd','computer']
-  input_select_list=[]
-  for input_val in input_values:
-      input_select_list.append((input_val.capitalize(),input_val))
   for par in original_pars:
       value=getattr(gc,par)
       value=getattr(gc,par)
@@ -103,8 +107,8 @@ def make_smenu(smenu):
           widget=smenu.add.text_input(par+': ',default=value,onchange=set_input,args=[par],input_type=pygame_menu.locals.INPUT_INT,font_size=font_size,align=pygame_menu.locals.ALIGN_LEFT)
       elif type(value) is float:
           widget=smenu.add.text_input(par+': ',default=value,onchange=set_input,args=[par],input_type=pygame_menu.locals.INPUT_FLOAT,font_size=font_size,align=pygame_menu.locals.ALIGN_LEFT)
-      elif value in input_values:
-          widget=smenu.add.dropselect(title=par+': ',items=input_select_list,onchange=set_input_drop,args=[par],default=input_values.index(value),font_size=font_size,align=pygame_menu.locals.ALIGN_LEFT)
+#      elif value in input_values:
+#          widget=smenu.add.dropselect(title=par+': ',items=input_select_list,onchange=set_input_drop,args=[par],default=input_values.index(value),font_size=font_size,align=pygame_menu.locals.ALIGN_LEFT)
       elif type(value) is tuple:
           tmenu=pygame_menu.Menu(par,600,400,theme=pygame_menu.themes.THEME_DEFAULT)
           widget=smenu.add.button(par,tmenu,font_size=font_size,align=pygame_menu.locals.ALIGN_LEFT)
